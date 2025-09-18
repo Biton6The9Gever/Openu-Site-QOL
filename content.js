201e65c5bf8053ab07993c92cc84c96d90f7ee99
Yehonatan Biton <yehonatanbiton4@gmail.com>
2025-09-19 02:02:20 +0300

    Added a new blocked word to remove its row

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ const rows = document.querySelectorAll("table.content_tbl tbody tr");
 // Loop through each row
 rows.forEach(row => {
     const statusCell = row.cells[3]; // 4th column is status
-    if (statusCell && (statusCell.textContent.includes("מבוטל") || statusCell.textContent.includes("הוחלף") || statusCell.textContent.includes("נדחה"))) {
+    if (statusCell && (statusCell.textContent.includes("מבוטל") || statusCell.textContent.includes("אי עמידה בדרישות")|| statusCell.textContent.includes("הוחלף") || statusCell.textContent.includes("נדחה"))) {
         row.remove(); // remove the row from the table
     }
 });
